871fe2524a16c31007208e0d5a2bf11655bd13b6
hhosseini1377 <hhosseini@ce.sharif.edu>
2020-08-01 19:32:20 +0430

    profile for patient completed

--- doctor/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Doctor(models.Model):
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
-    speciality = models.CharField(max_length=20)
-    level = models.CharField(max_length=20)
--- doctor/serlializers.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-from rest_framework import serializers
-from .models import Doctor
-
-
-class DoctorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Doctor
-        fields = '__all__'

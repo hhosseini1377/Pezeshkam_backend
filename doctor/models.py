74e02937713dbe806d04535aafe2f6d3016067f0
hhosseini1377 <hhosseini@ce.sharif.edu>
2020-08-01 15:29:04 +0430

    first commit

---
+++ doctor/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Doctor(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    speciality = models.CharField(max_length=20)
+    level = models.CharField(max_length=20)
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/hossein-hosseini/Desktop/mobile_developing/project/backend/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
